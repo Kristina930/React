7b4ec443da02a27ae023d43b30886f0a9271e602
Кристина Медянникова <72264341+Kristina930@users.noreply.github.com>
2022-09-21 16:24:06 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+
+const [messageList, setMessageList] = useState([]);
+const [text, setText] = useState('');
+const [author, setAuthor] = useState('');
+
+const handleClick = (event) => {
+  event.preventDefault();
+  setMessageList(prevState => [...prevState, {
+    author: author, 
+    text: text
+  }]);
+}
+
+
+useEffect(() => {
+  setTimeout(() => {
+    botList(messageList);
+    }, 
+    1000);
+});
+
+function botList() {
+  const itemAuthor = messageList[messageList.length - 1];
+    if(itemAuthor && itemAuthor.author) {
+      setMessageList(prevState => [
+        ...prevState, {
+          //Не выводить имя автора, выводить ${itemAuthor.author}, не понимаю почему.
+            // eslint-disable-next-line no-template-curly-in-string
+            text: 'Сообщение от автора ${itemAuthor.author}'
+          }
+        ])
+    }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={handleClick}>
+          Ведите текст
+          <input value={text} onChange={(event) => setText(event.target.value)}/><br/>
+          Ведите автора:
+          <input value={author} onChange={(event) => setAuthor(event.target.value)}/><br/>
+          <button type="submit">Добавить сообщение</button>
+  
+        </form>
+        {messageList.map((message) => {
+          return(
+          <div>
+            {message.text}
+            {message.author}
+          </div>
+          )
+      })}
     </div>
   );
 }
+
 
 export default App;
