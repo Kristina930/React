82167c68c1b70e50acf8355f3540b8812b1785c6
Кристина Медянникова <72264341+Kristina930@users.noreply.github.com>
2022-09-22 18:17:24 +0300

    lesson3

--- MessageList.js
+++ MessageList.js
@@ -4,7 +4,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 
 
@@ -12,7 +11,7 @@ function MessageList({array}) {
     const theme = useTheme();
     if (array.length > 0) {
         return (
-            <List sx={{width: '100%', maxWidth: 200, bgcolor: theme.palette.primary.background, borderRadius: 3}}>
+            <List sx={{width: '100%', maxWidth: 200, bgcolor: theme.palette.primary.background}}>
                 {array.map(item => (
                     <ListItem  key={item.id}>
                         <ListItemAvatar>
@@ -20,35 +19,7 @@ function MessageList({array}) {
                             <Avatar alt={item.user.toUpperCase()} src="/static/images/avatar/2.jpg"/>
                             <Avatar alt={item.user.toUpperCase()} src="/static/images/avatar/3.jpg"/>
                         </ListItemAvatar>
-                        <ListItemText
-                            primary={
-                                <React.Fragment>
-                                    <Typography
-                                        sx={{display: 'inline', color: 'orange'}}
-                                        component="span"
-                                        color="text.primary.main"
-                                    >
-                                        {item.user} wrote
-                                    </Typography>
-                                    <Typography
-                                        sx={{display: 'inline', color: 'black'}}
-                                        component="span"
-                                        color="text.primary.main"
-                                    >
-                                        {item.user} wrote
-                                    </Typography>
-                                    <Typography
-                                        sx={{display: 'inline', color: 'pink'}}
-                                        component="span"
-                                        color="text.primary.main"
-                                    >
-                                        {item.user} wrote
-                                    </Typography>
-                                </React.Fragment>
-                                
-                            }
-                            
-                        />
+                        <ListItemText />
                     </ListItem>
                     ))}
                     </List>
--- src/Chat.js
+++ src/Chat.js
@@ -4,7 +4,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 
 
@@ -12,7 +11,7 @@ function Chat({Chat}) {
     const theme = useTheme();
     if (Chat.length > 0) {
         return (
-        <List sx={{ width: '100%', maxWidth: 450, bgcolor: theme.palette.primary.main, borderRadius:2 }}>
+        <List sx={{ width: '100%', maxWidth: 450, bgcolor: theme.palette.primary.main }}>
             {Chat.map(item => (
                 <ListItem alignItems="flex-start" key={item.id}>
                     <ListItemAvatar>
@@ -20,34 +19,7 @@ function Chat({Chat}) {
                             <Avatar alt={item.user.toUpperCase()} src="/static/images/avatar/2.jpg"/>
                             <Avatar alt={item.user.toUpperCase()} src="/static/images/avatar/3.jpg"/>
                     </ListItemAvatar>
-                    <ListItemText
-                        primary={
-                        <React.Fragment>
-                            <Typography
-                                sx={{ display: 'block', color: 'orange' }}
-                                component="span"
-                                color="text.primary.main"
-                            >
-                                {item.user}
-                            </Typography>
-                            <Typography
-                                sx={{ display: 'block', color: 'black' }}
-                                component="span"
-                                color="text.primary.main"
-                            >
-                                {item.user}
-                            </Typography>
-                            <Typography
-                                sx={{ display: 'block', color: 'pink' }}
-                                component="span"
-                                color="text.primary.main"
-                            >
-                                {item.user}
-                            </Typography>
-                        </React.Fragment>
-                    }
-
-                    />
+                    <ListItemText />
                 </ListItem>
                 ))}
         </List>
